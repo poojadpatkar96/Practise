81a44a0ad0e5e595ace22421a447d1df631625bd
poojadpatkar96 <69250646+poojadpatkar96@users.noreply.github.com>
2020-09-18 12:14:41 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -3,11 +3,9 @@ public class AddTwoNumbers {
    public static void main(String[] args) {
         
 
-      int num1 = 50, num2 = 150, num3=94,sum;
-=======
-      int num1 = 50, num2 = 150, num3=34,sum;
+      int num1 = 10, num2 = 25, num3=98, sum;
 
-      sum = num1 + num2+num3;
+          sum = num1 + num2+num3;
 
       System.out.println("Sum of these numbers: "+sum);
    }
