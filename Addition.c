f8284d1fec53f7df41d30f4d7938eae43c23440a
poojadpatkar96 <69250646+poojadpatkar96@users.noreply.github.com>
2020-10-27 10:52:27 +0530

    Update Addition.c

--- Addition.c
+++ Addition.c
@@ -8,16 +8,7 @@ int main()
   scanf("%d%d", &a1, &b, &c);
 
   sum = a1 + b + c;
-=======
-  int a, b,c, sum;
-
-  printf("Enter three numbers to add\n");
-  scanf("%d%d", &a, &b, &c);
-
-  sum = a + b+c;
-
-
   printf("Sum of the numbers = %d\n", sum);
   printf("sum is");
-  return 0;
+   printf("sum")
 }
